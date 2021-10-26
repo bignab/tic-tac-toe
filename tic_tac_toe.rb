f3726bcef55d383753c3ccf6ceb81208e58de76b
bignab <nabil.afnan.rafat@gmail.com>
2021-10-26 15:16:03 -0400

    Main game loop completed, have not checked for errors yet

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,8 +3,17 @@
 # Gameboard including the 3x3 array of tiles and methods to
 # evaluate whether there is a winning combination on the board
 class GameBoard
+  WINNING_COMBOS = [[[0, 0], [0, 1], [0, 2]],
+                    [[1, 0], [1, 1], [1, 2]],
+                    [[2, 0], [2, 1], [2, 2]],
+                    [[0, 0], [1, 0], [2, 0]],
+                    [[0, 1], [1, 1], [2, 1]],
+                    [[0, 2], [1, 2], [2, 2]],
+                    [[0, 0], [1, 1], [2, 2]],
+                    [[2, 0], [1, 1], [0, 2]]].freeze
   def initialize
     @game_tiles = Array.new(3) { Array.new(3) }
+    @player_turn = 'o'
     @game_tiles.each_with_index do |row, row_index|
       row.each_with_index do |_, column_index|
         @game_tiles[row_index][column_index] = GameTile.new(row_index, column_index)
@@ -27,25 +36,42 @@ class GameBoard
   def game_loop
     win_condition = false
     until win_condition
+      change_player_turn # @player_turn starts as 'o' such that during the first iteration 'x' goes first
+      get_player_input(@player_turn)
       print_board
-      
-      # evaluate_board
-      puts "Player 2: Enter your row input 'x'"
-      play_two_row = gets
-      puts "Player 2: Enter your column input 'x'"
-      play_two_column = gets
-      change_tile(play_two_row, play_two_column, 'x') if play_two_row.between?(0, 2) && play_two_column.between?(0, 2)
-      win_condition = true
+      win_condition = evaluate_board
     end
+    player = @player_turn == 'x' ? 1 : 2
+    puts "Congratulations Player #{player}, you've won!"
   end
-end
 
-def get_player_input(state)
-  puts "Player 1: Enter your row input '#{state}'"
-  play_one_row = gets
-  puts "Player 1: Enter your column input '#{state}'"
-  play_one_column = gets
-  change_tile(play_one_row, play_one_column, 'x') if play_one_row.between?(0, 2) && play_one_column.between?(0, 2)
+  def evaluate_board
+    WINNING_COMBOS.each do |combination|
+      count = 0
+      combination.each do |coordinate|
+        @game_tiles[coordinate[0]][coordinate[1]].tile_state == @player_turn ? count += 1 : 0
+      end
+      return true if count == 3
+    end
+    false
+  end
+
+  def change_player_turn
+    @player_turn = if @player_turn == 'x'
+                     'o'
+                   else
+                     'x'
+                   end
+  end
+
+  def get_player_input(state)
+    player = state == 'x' ? 1 : 2
+    puts "Player #{player}: Enter your row input '#{state}'"
+    play_one_row = gets.to_i
+    puts "Player #{player}: Enter your column input '#{state}'"
+    play_one_column = gets.to_i
+    change_tile(play_one_row, play_one_column, state) if play_one_row.between?(0, 2) && play_one_column.between?(0, 2)
+  end
 end
 
 # Class representing each of the nine game tiles in tic-tac-toe, with position_x and position_y
@@ -71,4 +97,4 @@ class GameTile
 end
 
 test_board = GameBoard.new
-p test_board.print_board
+test_board.game_loop
